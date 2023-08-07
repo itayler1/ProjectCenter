7e38c67c5089540564320d1f1e2362ceacb2b5ae
yehonatan604 <97249220+yehonatan604@users.noreply.github.com>
2023-08-07 15:15:49 +0300

    Add project files.

---
+++ gameCenter/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using gameCenter.Projects.Project1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gameCenter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Image)!.Opacity = 0.7;
+            if ((sender as Image)!.Name == "Image1")
+            {
+                GameText.Content = "Game No. 1 is a game about lorm ipsum & happy birthday";
+            }
+            if ((sender as Image)!.Name == "Image2")
+            {
+                GameText.Content = "Game No. 2 is a game about lorm ipsum & happy birthday";
+            }
+            if ((sender as Image)!.Name == "Image3")
+            {
+                GameText.Content = "Game No. 3 is a game about lorm ipsum & happy birthday";
+            }
+            if ((sender as Image)!.Name == "Image4")
+            {
+                GameText.Content = "Game No. 4 is a game about lorm ipsum & happy birthday";
+            }
+            if ((sender as Image)!.Name == "Image5")
+            {
+                GameText.Content = "Game No. 5 is a game about lorm ipsum & happy birthday";
+            }
+            if ((sender as Image)!.Name == "Image6")
+            {
+                GameText.Content = "Game No. 6 is a game about lorm ipsum & happy birthday";
+            }
+        }
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Image)!.Opacity = 1;
+            GameText.Content = "please pick a game";
+        }
+
+        private void Image1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Project1 project1 = new();
+            project1.ShowDialog();
+        }
+    }
+}
---
+++ gameCenter/Projects/Project1/Project1.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.Models;
+
+namespace gameCenter.Projects.Project1
+{
+    /// <summary>
+    /// Interaction logic for Project1.xaml
+    /// </summary>
+    public partial class Project1 : Window
+    {
+        List<User> Users { get; set; }
+        public Project1()
+        {
+            Users = new()  {
+                new User()
+                {
+                    Id = 1,
+                    Name = "Joe",
+                    Email = "joe@email.com"
+                },
+                new User()
+                {
+                    Id = 2,
+                    Name = "Bob",
+                    Email = "bob@email.com"
+                },
+                new User()
+                {
+                    Id = 3,
+                    Name = "Gil",
+                    Email = "gil@email.com"
+                },
+                new User()
+                {
+                    Id = 4,
+                    Name = "Shosh",
+                    Email = "shosh@email.com"
+                }
+        };
+
+            InitializeComponent();
+            usersList.ItemsSource = Users;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Users.Add(new User()
+            {
+                Name = userName.Text,
+                Email = userEmail.Text,
+                Id = Users.Count + 1
+            });
+
+            usersList.ItemsSource = Users.ToList();
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            userEmail.Text = string.Empty;
+            userName.Text = string.Empty;
+        }
+    }
+}
